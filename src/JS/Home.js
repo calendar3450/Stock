c6c4475a7d8f8b2c33f540a785fe0879d24792df
calendar3450 <mjk3450@naver.com>
2022-12-15 19:43:55 +0900

    파프리카 api 버리고 업비트로 통일
    홈에 있는 주요 코인 가격 이랑 사는 페이지의 가격이 달라서 api통일

--- src/JS/Home.js
+++ src/JS/Home.js
@@ -2,14 +2,19 @@ import {useState,useEffect} from "react"
 import { Link } from "react-router-dom";
 import Location from './CSS/Location.module.css';
 import Main from './CSS/MainColor.module.css';
-//실시간 가격 변동
+//실시간 가격 변동, cors문제
 
 const Home = ()=> {
     const [coins,setCoins]=useState([]);
     const [loading,isLoading]=useState(true);
-    const [paprikaLoading,isPaprikaLoading]=useState(true);
-    const [paprikaCoin,setpaprikaCoin]=useState([]);
     const [haveMoney, setHaveMoney] = useState(100000000);
+    const [coinBTC,setBTC] = useState([]);
+    const [coinETH,setETH] = useState([]);
+    const [coinXRP,setXRP] = useState([]);
+    const [bitLoading,isBitLoading]= useState(true);
+    const [ethLoading,isEthLoading]= useState(true);
+    const [xrpLoading,isXrpLoading]= useState(true);
+    
 
     //업비트로 리스트 뽑기.
     useEffect(()=> {
@@ -18,35 +23,56 @@ const Home = ()=> {
         .then(response => {
           setCoins(response);
           isLoading(false);
+          
         })
-      },[])
-    //파프리카로 주요 코인 가격 보기.
-    useEffect(() => {
-      fetch('https://api.coinpaprika.com/v1/tickers?quotes=KRW')
-      .then(response => response.json())
-        .then(response => {
-          setpaprikaCoin(response);
-          isPaprikaLoading(false);
-    })
-  },[])
 
+      },[])
+
+    //업비트로 비트코인 가격 보기.
+    useEffect(() =>{
+      fetch(`https://api.upbit.com/v1/trades/ticks?market=KRW-BTC&count=1`)
+      .then(response => response.json())
+      .then(response => {
+        setBTC(response);
+        isBitLoading(false);
+      })
+    },[])
+
+    //업비트로 이더리움 가격 보기.
+    useEffect(() =>{
+      fetch(`https://api.upbit.com/v1/trades/ticks?market=KRW-ETH&count=1`)
+      .then(response => response.json())
+      .then(response => {
+        setETH(response);
+        isEthLoading(false);
+      })
+    },[])
+
+    //업비트로 이더리움 가격 보기.
+    useEffect(() =>{
+      fetch(`https://api.upbit.com/v1/trades/ticks?market=KRW-XRP&count=1`)
+      .then(response => response.json())
+      .then(response => {
+        setXRP(response);
+        isXrpLoading(false);
+      })
+    },[])
       return(
         <div className={Main.Entire}>
         <Link to='/' style={{textDecoration: 'none'}}>
         <a className={Main.Main}>코인 시세를 보자구요!</a>
         </Link>
         <p/>
-        
         <div className={Main.MainCoinNCoinList}>
-          {paprikaLoading ? (<h2>로딩중 입니다...</h2>):
+          {bitLoading || ethLoading || xrpLoading ? (<h2>로딩중 입니다...</h2>):
           <div className={Main.MainCoinsCollection}>
           <h1 className={Location.MainCoin}>주요 코인 시세 24H</h1>
-          <h2 className={Location.MainCoin}>BitCoin,비트코인: {paprikaCoin[0].quotes.KRW.percent_change_24h}%</h2>
-          <h3 className={Location.MainCoin}>시세:{Math.ceil(paprikaCoin[0].quotes.KRW.price).toLocaleString()}\</h3>
-          <h2 className={Location.MainCoin}>Ethereum,이더리움: {paprikaCoin[1].quotes.KRW.percent_change_24h}%</h2>
-          <h3 className={Location.MainCoin}>시세:{Math.ceil(paprikaCoin[1].quotes.KRW.price).toLocaleString()}\</h3>
-          <h2 className={Location.MainCoin}>Ripple 리플: {paprikaCoin[6].quotes.KRW.percent_change_24h}%</h2>
-          <h3 className={Location.MainCoin}>시세:{Math.ceil(paprikaCoin[6].quotes.KRW.price).toLocaleString()}\</h3>
+          <h2 className={Location.MainCoin}>BitCoin,비트코인: {(coinBTC[0].change_price /coinBTC[0].prev_closing_price *100).toFixed(2)}%</h2>
+          <h3 className={Location.MainCoin}>시세:{coinBTC[0].trade_price.toLocaleString()}\</h3>
+          <h2 className={Location.MainCoin}>Ethereum,이더리움: {(coinETH[0].change_price /coinETH[0].prev_closing_price *100).toFixed(2)}%</h2>
+          <h3 className={Location.MainCoin}>시세:{coinETH[0].trade_price.toLocaleString().toLocaleString()}\</h3>
+          <h2 className={Location.MainCoin}>Ripple 리플: {(coinXRP[0].change_price /coinXRP[0].prev_closing_price *100).toFixed(2)}%</h2>
+          <h3 className={Location.MainCoin}>시세:{coinXRP[0].trade_price.toLocaleString().toLocaleString()}\</h3>
           </div>
           }
         <div className={Location.List}>
