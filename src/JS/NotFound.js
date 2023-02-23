acb161c99b28ef1246ce016b298897e67ad833be
calendar3450 <75975464+calendar3450@users.noreply.github.com>
2023-02-23 23:31:57 +0900

    404페이지 css적용
    css적용

--- src/JS/NotFound.js
+++ src/JS/NotFound.js
@@ -1,7 +1,10 @@
+import style from './CSS/NotFound.module.css';
+import { useNavigate } from 'react-router-dom';
+
 const NotFound=()=> {
     return(
         <div>
-            <h1>404</h1>
+            <h1 className={style.Error}>404 Error</h1>
         </div>
     )
 }
