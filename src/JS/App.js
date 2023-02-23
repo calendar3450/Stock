8f09d39ecef9c9a888c27df36b8a5e44e3805d13
calendar3450 <mjk3450@naver.com>
2023-02-23 12:01:38 +0900

    404페이지 추가
    404페이지 추가함

--- src/JS/App.js
+++ src/JS/App.js
@@ -4,6 +4,7 @@ import Home from"./Home";
 import MinuteBitcoin from "./CoinMinute";
 import WeekBitcoin from "./CoinWeek";
 import DayBitcoin from "./CoinDay";
+import NotFound from "./NotFound";
 
 const App = () => {
   return(
@@ -14,7 +15,7 @@ const App = () => {
         <Route path='/coinMinute/:market' element={<MinuteBitcoin/>}/>
         <Route path='/coinWeek/:market' element={<WeekBitcoin/>}/>
         <Route path='/coinDay/:market' element={<DayBitcoin/>}/>
-
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </Router>
   )
